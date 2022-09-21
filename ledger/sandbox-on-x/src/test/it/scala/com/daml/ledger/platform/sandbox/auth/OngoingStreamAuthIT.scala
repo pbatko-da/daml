20a72db89bbafb24fbde3323b04e1c82122ba1fa
Pawel Batko <pawel.batko@digitalasset.com>
2022-09-21 12:17:03 +0200

    simplify cont'd

--- ledger/sandbox-on-x/src/test/it/scala/com/daml/ledger/platform/sandbox/auth/OngoingStreamAuthIT.scala
+++ ledger/sandbox-on-x/src/test/it/scala/com/daml/ledger/platform/sandbox/auth/OngoingStreamAuthIT.scala
@@ -243,7 +243,7 @@ final class OngoingStreamAuthIT
           ),
           updateMask = Some(
             FieldMask(
-              paths = Seq("user.is_deactivated")
+              paths = Seq("is_deactivated")
             )
           ),
         )
